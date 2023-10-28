c74496e76dc1423f3ed3066dc20f4fbe2382e7ff
CamillaWan <camillawan99@gmail.com>
2023-10-28 23:17:15 +1100

    play with React Render HTML

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello again React!</h1>
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container);
+root.render(<p>Hello</p>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
