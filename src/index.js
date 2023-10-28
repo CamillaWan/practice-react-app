d4e74b5fc6531ff6ac36ae579816af2d241914ee
CamillaWan <camillawan99@gmail.com>
2023-10-29 00:43:33 +1100

    play render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+ 
+const myComponent = (
+    <ul>
+        <li>Learn React</li>
+        <li>Create a weather app with React</li>
+        <li>Join a P3 group</li>
+        <li>Finish my P1</li>
+    </ul>
+)
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container);
-root.render(<p>Hello</p>);
+root.render(myComponent);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
