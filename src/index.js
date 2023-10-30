a20d5227a7051fd9d7b874534ca800bb4cb9ba70
CamillaWan <camillawan99@gmail.com>
2023-10-30 18:04:31 +1100

    Play with [props] and [Components in Components]

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Kid() {
-    return <h2>I'm a smart girl.</h2>
+function Kid(props) {
+    return <h2>A {props.personality} girl!</h2>
+}
+
+function Kindergarden() {
+    return (
+    <>
+      <h1>Who is/are in my Kindergarden?</h1>
+      <Kid personality="naughty"/> 
+    </>
+    );
 }
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<Kid />);
+root.render(<Kindergarden />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
