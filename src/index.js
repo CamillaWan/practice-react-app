14505b907ebc33f10ae9e5c20e913da53eb8d7ad
CamillaWan <camillawan99@gmail.com>
2023-10-30 15:47:51 +1100

    play with class & function components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const x = 9;
-const myComponent = (
-   <div>{(x)>8?
-    <ul>
-        <li>Learn React</li>
-        <li>Create a weather app with React</li>
-        <li>Join a P3 group</li>
-        <li>Finish my P1</li>
-    </ul> : "No Way"}
-   </div>  
-)
+function Kid() {
+    return <h2>I'm a smart girl.</h2>
+}
+
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(myComponent);
+root.render(<Kid />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
