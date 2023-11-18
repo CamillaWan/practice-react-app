4b4a144bc34310ff21f1b4391b869069cc69c264
CamillaWan <camillawan99@gmail.com>
2023-11-18 21:27:43 +1100

    review and practice props

--- src/Kindergarden.js
+++ src/Kindergarden.js
@@ -1,12 +1,13 @@
 function Kid(props) {
-    return <h2>A {props.personality} girl!</h2>
+    return <h2>A {props.student.personality} girl who is {props.student.name}!</h2>
 }
 
 function Kindergarden() {
+    const firstGirl = {name: "Camilla", personality: "clever"};
     return (
     <>
       <h1>Who is/are in my Kindergarden?</h1>
-      <Kid personality="naughty"/> 
+      <Kid student= {firstGirl}/> 
     </>
     );
 }
